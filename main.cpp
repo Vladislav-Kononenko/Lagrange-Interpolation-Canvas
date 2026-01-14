105862223a795957b16444431c115bdd81b66b5c
Vladislav-Kononenko <vlad.konon2005@mail.ru>
2026-01-14 19:43:44 +0300

    Написал функцию которая генерирует точки

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+struct Point {
+    double x;
+    double y;
+};
+
+
+vector<Point> generate_points(
+    int m,
+    double x_min,
+    double x_max
+) {
+    vector<Point> points;
+
+    if (m < 2) {
+        return points;
+    }
+
+    double h = (x_max - x_min) / (m - 1);
+
+    for (int i = 0; i < m; ++i) {
+        double x = x_min + i * h;
+        double y = x * x;
+
+        points.push_back({x, y});
+    }
+
+    return points;
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    for (const auto& point : generate_points(3, 100, 1000)) {
+        std::cout << "Point: (" << point.x << ", " << point.y << ")" << std::endl;
+    }
+
     return 0;
 }
