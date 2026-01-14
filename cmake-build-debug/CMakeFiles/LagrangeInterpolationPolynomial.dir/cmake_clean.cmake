7220253896463a0d72d436e99d42619ddbf9fffa
Vladislav-Kononenko <vlad.konon2005@mail.ru>
2026-01-14 21:37:43 +0300

    Написал шаблон проекта

---
+++ cmake-build-debug/CMakeFiles/LagrangeInterpolationPolynomial.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LagrangeInterpolationPolynomial.dir/link.d"
+  "CMakeFiles/LagrangeInterpolationPolynomial.dir/src/Canvas.cpp.o"
+  "CMakeFiles/LagrangeInterpolationPolynomial.dir/src/Canvas.cpp.o.d"
+  "CMakeFiles/LagrangeInterpolationPolynomial.dir/src/Interpolator.cpp.o"
+  "CMakeFiles/LagrangeInterpolationPolynomial.dir/src/Interpolator.cpp.o.d"
+  "CMakeFiles/LagrangeInterpolationPolynomial.dir/src/main.cpp.o"
+  "CMakeFiles/LagrangeInterpolationPolynomial.dir/src/main.cpp.o.d"
+  "LagrangeInterpolationPolynomial"
+  "LagrangeInterpolationPolynomial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LagrangeInterpolationPolynomial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
